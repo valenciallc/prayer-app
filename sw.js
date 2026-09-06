60e3418f97ee152e991874494bddca7761516915
valenciallc <valenciallchr@gmail.com>
2026-09-06 09:05:37 +0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE="prayer-v9";
+const ASSETS=["index.html","manifest.json","icon.svg","logo.png","السريحي.mp3","اقامة.mp3"];
+self.addEventListener("install",e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+self.addEventListener("activate",e=>{
+  e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener("fetch",e=>{
+  if(e.request.method!=="GET")return;
+  e.respondWith(
+    caches.match(e.request).then(hit=>{
+      if(hit)return hit;
+      return fetch(e.request).then(res=>{
+        const copy=res.clone();
+        if(res.ok&&e.request.url.startsWith(self.location.origin))caches.open(CACHE).then(c=>c.put(e.request,copy));
+        return res;
+      }).catch(()=>caches.match("index.html"));
+    })
+  );
+});
